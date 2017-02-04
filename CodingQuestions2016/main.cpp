567c4012806a5007a4d4c06e07100e10df9edace
Rippler <lfang68@gmail.com>
2017-02-04 14:51:13 -0800

    Initial Commit

---
+++ CodingQuestions2016/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CodingQuestions2016
+//
+//  Created by Fang Liu on 2/4/17.
+//  Copyright © 2017 Fang Liu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
